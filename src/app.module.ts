ec8f4d1e5e5cc178e6d5af199e1ec4936336d9d8
Arnold Jabea <aroljabea71@gmail.com>
2025-02-12 13:42:46 +0100

    creation des composants principaux

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { TablesController } from './tables/tables.controller';
 import { TablesService } from './tables/tables.service';
 import { TablesModule } from './tables/tables.module';
+import { UserModule } from './user/user.module';
+import { GameModule } from './game/game.module';
 
 @Module({
-  imports: [TablesModule],
+  imports: [TablesModule, UserModule, GameModule],
   controllers: [AppController, TablesController],
   providers: [AppService, TablesService],
 })
