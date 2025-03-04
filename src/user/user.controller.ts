f229fa5a41c6663fffad369eb6237f00fd5fc610
Singuila01 <mbayechris201@gmail.com>
2025-03-04 16:05:44 +0100

    Modification du controlleur (suppression pour le DTO)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -19,19 +19,6 @@ export class UserController {
         return `Propriété numéro #${id}`;
     }
 
-    @Post()
-    async create(@Body() body : UserDto) {
-        const user = await this.userService.findByUsername(body.username);
-        if (user != undefined) {
-            console.log(user)
-            throw new BadRequestException('Ce nom est déjà utilisé.')
-        }
-        this.userService.createUser(body.username, body.password)
-
-        // await this.userService.signUp(body.email, body.password)
-
-        // await this.ownersService.signIn(body.name, body.password)
-    }
-
+    
     
 }
